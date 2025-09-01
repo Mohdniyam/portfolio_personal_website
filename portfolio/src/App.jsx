bc659316d762e51521986fefc1fde38147a964d6
moaaz-cse <moaazcse@gmail.com>
2025-09-01 14:53:36 +0530

    conflict resolved

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css'
 
 function App() {
-
-
   return (
     <>
       <div>
-<<<<<<< Updated upstream
-       <h1>Welcome to Jamia Milia Islamia</h1>
-=======
         <h1 className="text-red-500 font-bold underline">Hello world! From App.jsx</h1>
       </div>
       <div>
@@ -18,7 +13,6 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
->>>>>>> Stashed changes
       </div>
     
     </>
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      cd,
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        brandWhite: "#FFFFFF",
+        brandGrey: {
+          light: "#F5F5F5",
+          DEFAULT: "#9CA3AF", // Tailwind’s gray-400-ish
+          dark: "#4B5563", // Tailwind’s gray-600-ish
+        },
+      },
+    },
+  },
+  plugins: [],
+};
