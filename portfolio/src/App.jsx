1c37f879d26f716ee73ddac20964fcb7939a08bf
moaaz-cse <moaazcse@gmail.com>
2025-09-01 14:36:33 +0530

    tailwind added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,7 +6,19 @@ function App() {
   return (
     <>
       <div>
+<<<<<<< Updated upstream
        <h1>Welcome to Jamia Milia Islamia</h1>
+=======
+        <h1 className="text-red-500 font-bold underline">Hello world! From App.jsx</h1>
+      </div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+>>>>>>> Stashed changes
       </div>
     
     </>
