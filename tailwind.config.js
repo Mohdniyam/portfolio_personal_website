f8e91bbe7ba30c6f5cd07b3e5d318dcb2f0e3857
moaaz-cse <moaazcse@gmail.com>
2025-09-01 14:40:48 +0530

    changes in config file

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // ✅ Default font everywhere
+      },
+      colors: {
+        brandWhite: "#FFFFFF",
+        brandGrey: {
+          light: "#F5F5F5",
+          DEFAULT: "#9CA3AF", // Tailwind’s gray-400-ish
+          dark: "#4B5563", // Tailwind’s gray-600-ish
+        },
+      },
+    },
+  },
+  plugins: [],
+};
